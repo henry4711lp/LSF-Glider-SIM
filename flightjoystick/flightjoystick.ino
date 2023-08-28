aa4d557b2f5d7c729d24a472647b651cf74b0824
Stefan Hagemann <stefan.hagemann@vodafone.com>
2023-08-28 10:41:19 +0200

    USB Tastatur Emulation hinzugefügt. Zum "Schnell-Reperatur" mit der einer Taste

--- flightjoystick/flightjoystick.ino
+++ flightjoystick/flightjoystick.ino
@@ -1,4 +1,5 @@
 #include "Joystick.h"
+#include "Keyboard.h"
 
 Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, 
   JOYSTICK_TYPE_MULTI_AXIS, 32, 0,
@@ -10,6 +11,9 @@ void setup() {
 
 
  Joystick.begin(false);
+ Keyboard.begin();
+
+  pinMode(2, INPUT_PULLUP);
 
 
   Joystick.setXAxisRange(0, 500);
@@ -29,6 +33,19 @@ void loop() {
   Joystick.setThrottle(analogRead(A1));
 
   Joystick.sendState();
+
+  if (!digitalRead(2))
+  {
+      delay(200);
+      Keyboard.print("qq");
+      delay(20);
+      Keyboard.press(KEY_F1);
+      delay(20);
+      Keyboard.releaseAll();
+      delay(500);  
+  }
+
+
   
 
 }
